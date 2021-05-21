264613dffb76666f113ae2632110ad102ceb7b86
Zac Freeman <zachary.freeman@autozone.com>
2021-05-21 12:22:17 -0500

    updated latency-test.sh to make requests at regular intervals, added reviewers, noted store VM used for testing

--- latency-test.sh
+++ latency-test.sh
@@ -8,17 +8,24 @@ fi
 
 url="$1"
 output="$2"
+interval=0.5
 num=100
-if [[ $# -eq 3 ]]
+if [[ $# -ge 3 ]]
 then
-    num="$3"
+    interval="$3"
 fi
 
-echo "recording response times for $num requests to $url into $output..."
+if [[ $# -eq 4 ]]
+then
+    num="$4"
+fi
+
+echo "recording response times for $num requests to $url made every $interval into $output..."
 
 rm "$output"
 
 for (( count=0; count<num; count++ ))
 do
-    curl --insecure --silent --output "/dev/null" --write-out "%{time_total}\n" "$url" >> "$output"
+    curl --insecure --silent --output "/dev/null" --write-out "%{time_total}\n" "$url" >> "$output" &
+    sleep "$interval"
 done
